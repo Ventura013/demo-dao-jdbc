c3041a47010aa628f9fb21dd326b2984f17fcbb8
Diego Ventura <diego.venturaa89@gmail.com>
2023-01-30 20:33:47 -0300

    findByDepartment implementation

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import model.entities.Department;
 
-public interface DepartmantDao {
+public interface DepartmentDao {
 
 	void insert(Department dp);
 	void update(Department dp);
