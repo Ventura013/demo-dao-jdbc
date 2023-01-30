d80d446ca1a2bffc3f943255a3051853fd37b353
Diego Ventura <diego.venturaa89@gmail.com>
2023-01-30 17:05:46 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dp = new Department(10, "Books");
+		
+		System.out.println(dp);
+
+	}
+
+}
