09e0d4fa04fb0b8b420d3128f1f31336f74576ec
Diego Ventura <diego.venturaa89@gmail.com>
2023-01-30 17:48:58 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		Department dp = new Department(10, "Books");
 		
 		System.out.println(dp);
+		
+		Seller seller =  new Seller(17, "Diego", "diego@gmail.com", new Date(), 6000.00, dp);
+		
+		System.out.println(seller);
 
 	}
 
