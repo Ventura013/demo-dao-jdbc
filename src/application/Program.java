752b42e626661096259e1b7d34a62cb615f41c67
Diego Ventura <diego.venturaa89@gmail.com>
2023-01-30 19:34:01 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dp = new Department(10, "Books");
-		
-		System.out.println(dp);
-		
-		Seller seller =  new Seller(17, "Diego", "diego@gmail.com", new Date(), 6000.00, dp);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 
